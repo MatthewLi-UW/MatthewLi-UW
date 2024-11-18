7bb66f43b3e1bb2790beecd7b548493c601a2cb6
Matthew Li <matthewlischoolemail@gmail.com>
2024-11-18 00:48:48 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -18,13 +18,9 @@ import 'lenis/dist/lenis.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+  </StrictMode>,
 )
-
-// ReactDOM.render((
-//    <BrowserRouter basename={process.env.PUBLIC_URL}>
-//      <App />
-//    </BrowserRouter>
-// ), ...)
