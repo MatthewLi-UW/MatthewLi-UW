26c674cfd11df26c9f1f4621d7b0d92f06968566
Matthew Li <matthewlischoolemail@gmail.com>
2024-11-18 00:54:18 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@
 /* Node modules */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import BrowserRouter from 'react-router-dom'
 
 
 /* Components */
