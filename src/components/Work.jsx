2f56120ec334e179835784bb9a04386d407461b6
Matthew Li <matthewlischoolemail@gmail.com>
2024-11-18 16:38:55 -0500

    test

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -19,31 +19,19 @@ const works = [
       imgSrc: 'images/raiinet.png',
       title: 'RAIInet - Ability Chess',
       tags: ['Linux', 'C++', 'MVC'],
-      projectLink: 'https://pixstock-official.vercel.app/'
+      projectLink: ''
     },
     {
       imgSrc: 'images/project-3.jpg',
       title: 'Top Headlines - Web App',
       tags: ['JavaScript', 'API'],
+      projectLink: 'https://github.com/MatthewLi-UW/CountryNews'
+    },
+    {
+      imgSrc: 'images/anilens.png',
+      title: 'AniLens - Anime Recommender',
+      tags: ['Python', 'Machine Learning'],
       projectLink: ''
-    },
-    {
-      imgSrc: 'images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-      imgSrc: 'images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-      imgSrc: 'images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
     },
   ];
 
