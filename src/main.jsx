62487fae8e05ee42a2f9c0a93def9081970c13d8
Matthew Li <matthewlischoolemail@gmail.com>
2024-11-18 00:58:51 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import './index.css';
 import 'lenis/dist/lenis.css'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
      {/* <App /> */}
