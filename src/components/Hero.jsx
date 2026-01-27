3a3400a0a5436d24a3bd7b7a6c2e14aa4e5bbd67
Matthew Li <matthewli@verily.com>
2026-01-26 19:48:19 -0500

    update icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,14 +16,7 @@ const Hero = () => {
 
             <div>
                 <div className="flex items-center gap-3">
-                    <figure className="img-box w-9 h-9 rounded-lg">
-                        <img src={import.meta.env.BASE_URL + "/images/bageldolphin.png"}
-                        width={40}
-                        height={40}
-                        alt="Matthew Li Picture"
-                        className="img-cover"
-                        />
-                    </figure>
+
                     <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
                         <span className="relative w-2 h-2 rounded-full bg-emerald-400">
                             <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
